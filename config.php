7805fd507d40b76b385c996b5b976c92b692b20f
cicades <hyf1499812174@gmail.com>
2018-09-17 23:26:58 +0800

    start

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+	数据库主机地址
+*/
+define('DB_HOST', '127.0.0.1');
+/**
+	数据库用户名
+*/
+define('DB_USER_NAME', 'root');
+/**
+	数据库用户密码
+*/
+define('DB_PASSWORD', 'root');
+/**
+	数据库名字
+*/
+define('DB_NAME', 'baixiu');
